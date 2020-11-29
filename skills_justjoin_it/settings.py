43016924d1dcf0bf98bcd03e952483dbcbc97197
Adrian Wloszynski <42609894+wloszynski@users.noreply.github.com>
2020-11-29 18:48:07 +0100

    Update settings.py

--- skills_justjoin_it/settings.py
+++ skills_justjoin_it/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'd9n076@g_s=x+bwr63#mufz2)79qf-53pru%pf)==ueeyaie9v'
+SECRET_KEY = ''
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
