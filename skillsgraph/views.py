4d66fa278cee715bd1300a5ce453126967adc5e8
wloszynski <adrian@wloszynski.pl>
2020-12-07 19:27:50 +0100

    everything workin

--- skillsgraph/views.py
+++ skillsgraph/views.py
@@ -41,13 +41,15 @@ def dataPrint(request, category):
             sum_rest_rows += row[1]
         rows.append(('rest', sum_rest_rows))
 
-    categories_zipped = zip(categories, categories_menu)
 
     return render(request, 'skillsgraph/skills.html', {'categories':categories,'rows':rows,
- 'category_selected':category, 'all_rows':all_rows, 'categories_zipped':categories_zipped})
+ 'category_selected':category, 'all_rows':all_rows, 'categories_zipped':zip(categories, categories_menu)})
 
 def jobs(request):
-    
+    categories = ['all', 'javascript', 'html', 'php', 'ruby', 'python', 'java', 'net', 'scala', 'c', 'mobile', 'testing', 'devops', 'ux', 'pm', 'game', 'analytics', 'security', 'data', 'go', 'sap', 'support', 'other']
+    categories_menu = ['All', 'JS', 'HTML', 'PHP', 'Ruby', 'Python', 'Java', '.Net', 'Scala', 'C', 'Mobile', 'Testing', 'DevOps', 'UX/UI', 'PM', 'Game', 'Analytics', 'Security', 'Data', 'Go', 'SAP', 'Support', 'Other']
+
+
     now_date = str(datetime.datetime.now()).split(' ')[0]
     conn = sqlite3.connect('skill_counter_all.sqlite')
     cur = conn.cursor()
@@ -60,6 +62,7 @@ def jobs(request):
     
     rows = dict(cur.fetchall()[1:])
     rows = sorted(rows.items(), key=lambda x: x[1], reverse=True)
+    all_rows = rows.copy()
     
     if len(rows) > 35:
         rest_rows = rows[36:]
@@ -68,4 +71,7 @@ def jobs(request):
         for row in rest_rows:
             sum_rest_rows += row[1]
         rows.append(('rest', sum_rest_rows))
-    return render(request, 'skillsgraph/jobs.html', {'rows':rows})
+
+
+    return render(request, 'skillsgraph/jobs.html', {'rows':rows, 'categories':categories, 'categories_zipped':zip(categories, categories_menu)
+, 'all_rows':all_rows})
